599caa9ed5a8ae7a89022312482153c138c7f657
cmche <carolinecpink@gmail.com>
2020-03-10 09:00:50 -0400

    memories page update

--- object-meditation/website-template-with-external-css-images-js-responsive/script.js
+++ object-meditation/website-template-with-external-css-images-js-responsive/script.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
 
-  $("#mochiimg").click(function() {
-    $("body").css({
-      "background-color": "black"
-    });
 
+$("#mochiimg").click(function(){
+    $(".arya-pink-box").toggleClass("arya-blue-box");
   });
 
+$("#mochiimg").click(function(){
+    $(".nicole-blue-box").toggleClass("nicole-yellow-box");
+  });
 
+$("#mochiimg").click(function(){
+    $(".caroline-yellow-box").toggleClass("caroline-pink-box");
+  });
 
 
 });
