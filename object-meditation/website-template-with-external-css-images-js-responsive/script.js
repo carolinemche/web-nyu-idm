61f3369ef614b1e549b237cf371f92d0b721c639
cmche <carolinecpink@gmail.com>
2020-03-24 01:15:42 -0400

    final modifications, mobile breakpoints, 3 buttons

--- object-meditation/website-template-with-external-css-images-js-responsive/script.js
+++ object-meditation/website-template-with-external-css-images-js-responsive/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
 
+ // BOXES CHANGE COLOR WHEN MOCHI IS CLICKED
 $("#mochiimg").click(function(){
     $(".arya-pink-box").toggleClass("arya-blue-box");
   });
@@ -14,4 +15,28 @@ $("#mochiimg").click(function(){
   });
 
 
-});
+// BOX SHADOW ADDED TO HOME IMAGE BACKGROUND
+$("#backgroundButton").click(function(){
+    $(".homeimg").toggleClass("homeimg2");
+  });
+
+//CHRISTMAS STYLE WHEN CHRISTMAS IMAGES ARE CLICKED
+  $("#xmas").click(function() {
+    $("body").toggleClass("hide");
+  });
+
+  $("#xmas2").click(function() {
+    $("body").toggleClass("hide2");
+  });
+
+  $("#xmas3").click(function() {
+    $("body").toggleClass("hide3");
+  });
+
+//toggle for ipad screen, how we met button to show text
+$(".buttonToggle").click(function() {
+    $("#story").toggle("display");
+  });
+
+
+     });
