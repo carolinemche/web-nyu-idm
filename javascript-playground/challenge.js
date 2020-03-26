af920313167f216c423ae326b45b8177fe6d651f
cmche <carolinecpink@gmail.com>
2020-03-26 15:34:51 -0400

    challenge 6 and 7 completed

--- javascript-playground/challenge.js
+++ javascript-playground/challenge.js
@@ -39,14 +39,18 @@ let colors = ["pink", "blue", "yellow", "green", "blue"];
 // push your edits to github!!
 
 // loop to print out each color
-for (let i = 0; i < colors.length; i++) {
-  console.log(colors[i]);
-}
+// for (let i = 0; i < colors.length; i++) {
+//   console.log(colors[i]);
+// }
 
 
 // 6. Declare an empty string variable called 'text'. Each time you console.log a color from the above challenge number 2, add your color to this variable.
 // push your edits to github!!
-
+let colorText = "";
+for (let i = 0; i < colors.length; i++) {
+  colorText += colors[i] + "</br>";
+}
 
 //7. Create a div in the index.html with a unique id called whatever you want. Display the contents of your text variable to this div. Now you should have a list of 5 colors visible on your website.
 // push your edits to github!!
+$('#colors').html(colorText);
--- javascript-playground/demo.js
+++ javascript-playground/demo.js
@@ -16,16 +16,35 @@ function getRandom(max){
 // console.log(getRandom(3));
 //expected output is 0, 1,or 2 (not 3)
 
-
+//gets a random number within the length of nameArray
 let n = getRandom(namesArray.length);
+
 // console.log(n);
+//prints a random name
 let randomName = namesArray[n];
 console.log(randomName);
 
 
 let arrayAsString = namesArray.join(", ")
-console.log(arrayAsString);
-$('#names').html(arrayAsString);
+//prints all names
+console.log(arrayAsString)
+
+//sorts name array
+let sortedNames = namesArray.sort();
+console.log (sortedNames);
+
+let text = "";
+
+//loop that iterates through all the names
+for (i = 0; i < namesArray.length; i++){
+  //adds name of current index to the text and making a new line
+  text += namesArray[i] + " " + i +"</br>";
+}
+
+$('#listNames').html(text);
+
+// puts all names in the array on website/as html
+// $('#names').html(arrayAsString);
 
 // puts random name on website/as html
 // $('#name').html(randomName);
